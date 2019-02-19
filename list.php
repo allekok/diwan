ea5a3dcd8974ade891983968dc89206786ce30be
allekok <one@allekok.com>
2019-02-20 00:13:20 +0330

    ignore list-filenames.txt

--- list.php
+++ list.php
@@ -2,7 +2,7 @@
 
 function make_list ($path = ".") {
 
-    $not = [".", "..", "list.txt", "list.php", "list.php~", "image", "index.html", "index.html~", ".git", "LICENSE", "README.md", "google0a141cc724bea402.html", "sw.js", "sitemap.xml", "sitemap.xml~", "sitemap.php", "sitemap.php~", "desc", "list-filenames.php"];
+    $not = [".", "..", "list.txt", "list.php", "list.php~", "image", "index.html", "index.html~", ".git", "LICENSE", "README.md", "google0a141cc724bea402.html", "sw.js", "sitemap.xml", "sitemap.xml~", "sitemap.php", "sitemap.php~", "desc", "list-filenames.php", "list-filenames.txt"];
     $dir = opendir($path);
     $files = [];
     
@@ -22,16 +22,16 @@ function make_list ($path = ".") {
             }
 
             $files[] = [
-            "path" => $e,
-            "size" => number_format(filesize("$path/$e")/1000000, 1) . "MB",
-            "desc" => substr($desc,0,strlen($desc)-1),
+		"path" => $e,
+		"size" => number_format(filesize("$path/$e")/1000000, 1) . "MB",
+		"desc" => substr($desc,0,strlen($desc)-1),
             ];
         }
     }
 
     sort($files);
     for($i = 0; $i<count($files); $i++) {
-    $files[$i] = implode("\t\t", $files[$i]);
+	$files[$i] = implode("\t\t", $files[$i]);
     }
     $list = implode("\n\n", $files);
 
