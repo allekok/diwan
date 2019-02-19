a32802589fe1551de79dfeee912dcb96eb085c26
allekok <one@allekok.com>
2019-02-19 22:14:25 +0330

    add a program to list just filenames

---
+++ list-filenames.php
@@ -0,0 +1,34 @@
+<?php
+
+function make_list ($path = ".") {
+
+    $not = [".", "..", "list.txt", "list.php", "list.php~", "image", "index.html", "index.html~", ".git", "LICENSE", "README.md", "google0a141cc724bea402.html", "sw.js", "sitemap.xml", "sitemap.xml~", "sitemap.php", "sitemap.php~", "download.php~", "download.php", "sw.js~", "README.md~", ".gitignore", "1-list.php", "desc", "list-filenames.php", "list-filenames.txt"];
+    $dir = opendir($path);
+    $files = [];
+    
+    while (false !== ($e = readdir($dir))) {
+
+        if (! in_array($e, $not) ) {
+
+            if( is_dir("$path/$e") ) {
+
+                make_list ("$path/$e");
+            }
+
+            $files[] = $e;
+        }
+    }
+
+    sort($files);
+    
+    $list = implode("\n", $files);
+
+    $f = fopen("$path/list-filenames.txt", "w");
+    fwrite($f, $list);
+    fclose($f);
+
+    return true;
+}
+
+// run
+echo make_list() . "\n";
--- list.php
+++ list.php
@@ -2,7 +2,7 @@
 
 function make_list ($path = ".") {
 
-    $not = [".", "..", "list.txt", "list.php", "list.php~", "image", "index.html", "index.html~", ".git", "LICENSE", "README.md", "google0a141cc724bea402.html", "sw.js", "sitemap.xml", "sitemap.xml~", "sitemap.php", "sitemap.php~", "desc"];
+    $not = [".", "..", "list.txt", "list.php", "list.php~", "image", "index.html", "index.html~", ".git", "LICENSE", "README.md", "google0a141cc724bea402.html", "sw.js", "sitemap.xml", "sitemap.xml~", "sitemap.php", "sitemap.php~", "desc", "list-filenames.php"];
     $dir = opendir($path);
     $files = [];
     
