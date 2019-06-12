d48b0d2ea2a36d3a8c46cd13d9c576928d1ba40f
allekok <one@allekok.com>
2019-06-12 19:58:54 +0430

    [list.php] Code-styling

--- list.php
+++ list.php
@@ -1,40 +1,37 @@
 <?php
 /* Function */
-function make_list ($path = ".") {
+function make_list ($path = ".")
+{
     $not = [".", "..", "list.txt", "list.php", ".git",
-	    "LICENSE", "README.md", ".gitignore", "desc",
-	    "list-filenames.php", "list-filenames.txt"];
+	    "LICENSE", "README.md", ".gitignore", "desc"];
     $files = [];
     $dir = opendir($path);
     
-    while (false !== ($e = readdir($dir))) {
-
+    while (false !== ($e = readdir($dir)))
+    {
         if(in_array($e, $not))
-	    continue;
-	
+	    continue;	
         // Get book description
         $desc = "";
-        if(file_exists("$path/desc/$e.txt")) {
+        if(file_exists("$path/desc/$e.txt"))
+	{
             $desc = trim(file_get_contents("$path/desc/$e.txt"));
 	}
-
         $files[] = [
 	    "path" => $e,
 	    "size" => number_format(filesize("$path/$e")/1000000, 1) . "MB",
 	    "desc" => $desc,
         ];
     }
-
     sort($files);
-    for($i = 0; $i<count($files); $i++) {
+    for($i = 0; $i<count($files); $i++)
+    {
 	$files[$i] = implode("\t\t", $files[$i]);
-    }
-    
+    }    
     $list = implode("\n\n", $files);
     file_put_contents("$path/list.txt", $list);
 }
 
 /* Run */
 make_list();
-require('list-filenames.php');
 ?>
